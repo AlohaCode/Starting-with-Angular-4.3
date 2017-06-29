af0c2b3d41bcb3736b339bf15f7e02830ef2be6c
AlohaCode <bretthooper.maui@gmail.com>
2017-06-29 02:40:03 +0000

    v4.3 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { MoviesModule } from './modules/movies/movies.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { CharacterComponent } from './components/character/character.component';
+import { PageNotFoundComponent } from './components/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersListComponent,
+    CharacterComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MoviesModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
